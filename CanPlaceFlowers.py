38a8ecdbd466b86d36210f27e6029a0c24855512
ccartel <128412542+ccartel@users.noreply.github.com>
2023-03-20 15:30:52 -0400

    Update CanPlaceFlowers.py

--- CanPlaceFlowers.py
+++ CanPlaceFlowers.py
@@ -1,10 +1,25 @@
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+  
+  
 class Solution:
   def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-    for i, flower in enumerate(flowerbed):
-      if flower == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
-        flowerbed[i] = 1
+    for idx, dig in enumerate(flowerbed):
+      if dig == 0 and (idx == 0 or flowerbed[idx - 1] == 0) and (idx == len(flowerbed) - 1 or flowerbed[idx + 1] == 0):
+        flowerbed[idx] = 1
         n -= 1
       if n <= 0:
         return True
-
     return False
