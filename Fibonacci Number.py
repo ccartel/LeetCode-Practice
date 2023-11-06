1ba3b9ca287e07bffcf599749604cb777c5b26ba
Brian <128412542+ccartel@users.noreply.github.com>
2023-11-06 12:49:42 -0500

    Create Fibonacci Number.py

---
+++ Fibonacci Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+
+            dp = [0] * n
+            dp[0] = 0
+            dp[1] = 1
+        for i in range(2, n):
+                dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1] + dp[n-2]
+        
